0d2aa044aa7f71db1de7c1d6b7954ec4fa44cad6
yuta-toshino1931 <yuta-toshino@hiroba1931.co.jp>
2025-07-05 21:17:05 +0900

    ベースパスを変更

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Mietoru_Demo/",
+  base: "/",
   publicDir: "public",
   server: {
     port: 5173,
